15529126c48a3d346d24106a677c10375c186807
shyusu4 <shyusupova4@gmail.com>
2023-01-17 16:50:27 +0500

    Fix linter errors

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-    belongs_to :user, class_name: 'User'
-    has_many :purchases, dependent: :destroy
+  belongs_to :user, class_name: 'User'
+  has_many :purchases, dependent: :destroy
 
   validates :name, presence: true
   validates :icon, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-         has_many :categories, dependent: :destroy
-         validates :name, presence: true
-         validates :email, presence: true, uniqueness: { case_sensitive: false }
-         validates :password, presence: true
+  has_many :categories, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
 end
